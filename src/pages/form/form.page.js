af331e3fa56edad950ee66f47be6effb1bc3f11a
Vitalii <vitalii.skorokhod@gmail.com>
2022-07-09 18:33:51 -0400

    Updated form page
    * updated flexify.css

--- src/pages/form/form.page.js
+++ src/pages/form/form.page.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default class FormPage extends React.Component {
-    state = { term: 'DEFAULT VALUE' };
+    state = { 
+        username: 'default_username',
+        password: ''
+    };
 
     render() {
         return (
@@ -9,9 +12,18 @@ export default class FormPage extends React.Component {
                 <h3>Form page</h3>
 
                 <form>
-                    <label className='fx-label'>Input with uppercased value: {this.state.term}</label>
-                    <input className='fx-input' type="text" value={this.state.term}
-                        onChange={(evt) => this.setState({ term: evt.target.value.toUpperCase() }) } />
+                    <div className='fx-u-mb-3'>
+                        <label className='fx-label'>Username with trimmed value (no spaces): [{this.state.username}]</label>
+                        <input className='fx-input' type="text" value={this.state.username}
+                            onChange={(evt) => this.setState({ username: evt.target.value.trim() }) } />
+                    </div>
+
+                    <div className='fx-u-mb-3'>
+                        <label className='fx-label'>Password with minimum lenght value: {this.state.password}</label>
+                        <input className='fx-input' type="password" value={this.state.password}
+                            onChange={(evt) => this.setState({ password: evt.target.value }) } />
+                        <div className='fx-u-color-danger'>{this.state.password.length < 4 ? 'Password must be at least 4 characters' : ''}</div>
+                    </div>
                 </form>
             </section>
         );
