8fef9ccbb7dce2150a4016c0198da3705f6f8d6c
Vitalii <vitalii.skorokhod@gmail.com>
2023-03-01 17:13:48 -0500

    Added flexify.css via npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/css/flexify/flexify.css';
+import 'flexify/css/flexify.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
