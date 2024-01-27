87f44b1718b3b510114423a1ef7c8572f8ee1f32
Vitalii <vitalii.skorokhod@gmail.com>
2024-01-26 20:16:55 -0500

    Added StarRating component

--- src/components/post-detail/post-detail.component.js
+++ src/components/post-detail/post-detail.component.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import StarRating from "../star-rating/star-rating.component";
 import "./post-detail.component.css";
 
 const PostDetail = () => {
@@ -17,6 +18,9 @@ const PostDetail = () => {
   return (
     <article>
       <h2>{post.title}</h2>
+
+      <StarRating />
+
       <div>{post.body}</div>
     </article>
   );
