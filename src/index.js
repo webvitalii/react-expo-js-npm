cc22cda727a69746c27f19db86df1848e5b3374f
Vitalii <vitalii.skorokhod@gmail.com>
2024-02-12 17:40:39 -0500

    Upgrade for react 18
    ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it’s running React 17. Learn more: https://reactjs.org/link/switch-to-createroot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import 'flexify/css/flexify.css'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "flexify/css/flexify.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.querySelector('#root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
