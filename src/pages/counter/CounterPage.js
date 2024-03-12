53005f815b909596b9a25e16e583d22d53526b2a
Vitalii <vitalii.skorokhod@gmail.com>
2024-03-12 18:35:37 -0400

    Cleanup CounterPage

--- src/pages/counter/CounterPage.js
+++ src/pages/counter/CounterPage.js
@@ -1,18 +1,6 @@
-import { useState, useReducer } from "react";
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "decrement":
-      return state - 1;
-    case "increment":
-      return state + 1;
-    default:
-      throw new Error();
-  }
-};
+import { useState } from "react";
 
 function CounterPage() {
-  const [countR, dispatch] = useReducer(counterReducer, 0);
   const [count, setCount] = useState(0);
 
   const decrementCount = () => {
@@ -33,23 +21,6 @@ function CounterPage() {
       <button onClick={incrementCount} className="fx-btn">
         Plus
       </button>
-
-      <div>
-        <h3>Count with useReducer: {countR}</h3>
-
-        <button
-          onClick={() => dispatch({ type: "decrement" })}
-          className="fx-btn"
-        >
-          Minus
-        </button>
-        <button
-          onClick={() => dispatch({ type: "increment" })}
-          className="fx-btn"
-        >
-          Plus
-        </button>
-      </div>
     </section>
   );
 }
